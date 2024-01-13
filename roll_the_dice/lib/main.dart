66838410e894ea1d367b0e4b767e8ea44a2b4a9f
Pattaradnai Kaeodumkoeng <kafaircoffee2434well@gmail.com>
2024-01-13 14:12:42 +0700

    add roll the dice

---
+++ roll_the_dice/lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          
+          title: Text(
+            'Roll the Dices',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.red,
+          centerTitle: true, // Center align the title
+          elevation: 5.0, // Add elevation to the app bar
+          shadowColor: Colors.black, // Set shadow colo
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+                
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+          //Get students to create the second die as a challenge
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
