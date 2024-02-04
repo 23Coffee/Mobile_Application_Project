a1f092f1d605b62422175ac9555a790e68231c8f
Pattaradnai Kaeodumkoeng <kafaircoffee2434well@gmail.com>
2024-02-04 17:38:14 +0700

    commit bmi cal

---
+++ bmi_calculator/lib/constants.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kActiveCardColourMale = Color.fromARGB(255, 110, 168, 255);
+const kActiveCardColourFemale = Color.fromARGB(255, 243, 116, 255);
+const kInactiveCardColour = Color(0xFF1D1E33);
+const kBottomContainerColour = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color.fromARGB(255, 255, 255, 255),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
